d50ad171887a296fb904df594acf98d4fae234db
wttk0304 <riruqwq@gmail.com>
2026-06-04 23:51:42 +0900

    Task2の中身を書いた

---
+++ 20260529/2/task02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+    int main()
+    {
+
+        int a;
+    
+        for (int a = 10; a >= 1; --a) {
+            cout << a << endl;
+        }
+    
+        return 0;
+    }
