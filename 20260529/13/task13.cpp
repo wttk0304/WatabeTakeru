ed20e7a1a1f0694bcd32c0e90a665a7d86bd066d
wttk0304 <riruqwq@gmail.com>
2026-06-05 01:37:05 +0900

    Task13の中身を記述
    カラコを用意

---
+++ 20260529/13/task13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+    int main() 
+    {
+        // 2進数 00101101 10111110 01100000
+        //       [  赤  ] [  緑  ] [  青  ]
+
+        // ANDで青だけ
+
+        int color = 0x2DBE60;
+    
+    
+    
+    
+    }
