1129c096268aca573d67d273be11d9de678f3fa3
wttk0304 <riruqwq@gmail.com>
2026-06-05 00:13:40 +0900

    よく見たら"1以上の"って部分がなかったから修正

--- 20260529/3/task03.cpp
+++ 20260529/3/task03.cpp
@@ -6,8 +6,10 @@ using namespace std;
 
         int a;
     
-        for (int a = 6; a <= 10; ++a) {
+        for (int a = 1; a <= 10; ++a) {
+            if (a >= 6) {
             cout << a << endl;
+            }
         }
     
         return 0;
