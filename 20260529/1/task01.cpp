8e42aea78b72a31dba16a95a3e0295386a266335
wttk0304 <riruqwq@gmail.com>
2026-06-04 23:56:11 +0900

    task1(?)

--- 20260529/1/task01.cpp
+++ 20260529/1/task01.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
-    int main (){
+    int main()
+    {
 
         int a;
 
         for (int a = 1; a <= 10; ++a) {
             cout << a << endl;
         }
-
+        
         return 0;
     }
