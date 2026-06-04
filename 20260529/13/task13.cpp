25a821d858b6ac97f236531864dde78a9729568c
wttk0304 <riruqwq@gmail.com>
2026-06-05 01:52:32 +0900

    忘れた...

--- 20260529/13/task13.cpp
+++ 20260529/13/task13.cpp
@@ -6,11 +6,13 @@ using namespace std;
         // 2進数 00101101 10111110 01100000
         //       [  赤  ] [  緑  ] [  青  ]
 
-        // ANDで青だけ
+        // ANDで青だけをくり抜く
+
+        //右に8つか16つずらす
 
         int color = 0x2DBE60;
     
-    
+        int blue = color & 0x0000FF;
     
     
     }
