2e9513f3fac753232d4e5524788073c6746335db
wttk0304 <riruqwq@gmail.com>
2026-06-05 00:11:02 +0900

    Task3の中身を書いた

---
+++ 20260529/3/task03.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+    int main()
+    {
+
+        int a;
+    
+        for (int a = 6; a <= 10; ++a) {
+            cout << a << endl;
+        }
+    
+        return 0;
+    }
