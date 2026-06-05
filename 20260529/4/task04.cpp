bbcfda9077cf3fddd735fa66ccdef26a5e341011
wttk0304 <riruqwq@gmail.com>
2026-06-05 16:51:46 +0900

    task04を再生成

---
+++ 20260529/4/task04.cpp
@@ -0,0 +1,14 @@
+ int main()
+    {
+
+    
+        int a;
+
+        for (int a = 1; a <= 10; ++a) {
+            if (a % 2 == 0) {
+            cout << a << endl;
+            }
+        }
+
+        return 0;
+    }
