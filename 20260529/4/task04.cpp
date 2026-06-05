96c6cfb6017c18e60486f82d484a713dae18d36e
wttk0304 <riruqwq@gmail.com>
2026-06-05 16:34:59 +0900

    Task04の中身を書いた

--- 20260529/4/task04.cpp
+++ 20260529/4/task04.cpp
@@ -4,6 +4,13 @@ using namespace std;
     int main()
     {
 
-    
+        int a;
+
+        for (int a = 1; a <= 10; ++a) {
+            if (a % 2 == 0) {
+            cout << a << endl;
+            }
+        }
+
         return 0;
     }
