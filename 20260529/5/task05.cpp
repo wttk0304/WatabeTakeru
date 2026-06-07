ee4f3b7440eeecd0ef03464a3f8a7b9a2cb76ac9
wttk0304 <riruqwq@gmail.com>
2026-06-07 18:58:55 +0900

    task05を記述,runしたexeを追加

--- 20260529/5/task05.cpp
+++ 20260529/5/task05.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
     int main()
     {
-
-
-
-
-
-
-
+        for (int a = 1; a <= 10; ++a) {
+            if (a % 3 != 0) {
+            cout << a << endl;
+            }
+        }
 
         return 0;
     }
