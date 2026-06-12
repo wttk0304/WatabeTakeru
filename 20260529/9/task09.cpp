a8c08f890d8223678ed80f10693fe7f0fe96a4ec
wttk0304 <riruqwq@gmail.com>
2026-06-12 17:36:04 +0900

    task09を記述

---
+++ 20260529/9/task09.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 100; ++i) {
+        if (i % 10 == 0 || i % 15 == 0) {
+            std::cout << i << std::endl;
+        }
+    }
+    
+    return 0;
+}
