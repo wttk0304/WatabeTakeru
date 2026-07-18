d20717df0ddd6d833d7e58d53f80a007192b36cb
wttk0304 <riruqwq@gmail.com>
2026-07-18 14:15:54 +0900

    ローカル定数に変更
    グローバルにする必要なかった気がするこの場合

--- 20260529/12/task12.cpp
+++ 20260529/12/task12.cpp
@@ -1,11 +1,13 @@
 #include <iostream>>
 using namespace std;
 
-const double TAX_RATE = 0.10;
-const int NORMAL_PRICE = 200;
-const int MEMBER_PRICE = 150;
-
 int main() {
     
+    const double TAX_RATE = 0.10;
+    const int NORMAL_PRICE = 200;
+    const int MEMBER_PRICE = 150;
+
+    int normal_tax = NORMAL_PRICE * TAX_RATE;
+    int normal_total = NORMAL_PRICE + normal_tax;
 
 }
