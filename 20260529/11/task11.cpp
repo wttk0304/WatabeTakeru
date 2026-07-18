f3a2a1e34a739b14ecccadda865d7fae9dd9e476
wttk0304 <riruqwq@gmail.com>
2026-07-18 14:23:12 +0900

    コメントアウトを削除した

--- 20260529/11/task11.cpp
+++ 20260529/11/task11.cpp
@@ -9,11 +9,9 @@ struct Define
 int main() {
     int price = 1500;
     
-    // 計算処理
     int tax = price * Define::TAX_RATE;  
     int total = price + tax;    
 
-    // 結果を出力
     cout << "定価    : " << price << " 円" << endl;
     cout << "消費税: " << tax << " 円" << endl;
     cout << "合計金額: " << total << " 円" << endl;
