bff23a51d46b8eb421452ab700e331c15acffef4
wttk0304 <riruqwq@gmail.com>
2026-07-18 16:47:03 +0900

    temp | 左の方が大きければ入れ替えるだからあってるはず

--- 20260619/1/Task01.cpp
+++ 20260619/1/Task01.cpp
@@ -11,4 +11,14 @@ using namespace std;
             array[i] = rand() % 100 + 1; 
         }
 
+        for (int i = 0; i < SIZE - 1; i++) {
+            for (int j = 0; j < SIZE - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
     }
