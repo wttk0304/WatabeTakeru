a94f0bd2a800abddb7ed208ba779142d8729429d
wttk0304 <riruqwq@gmail.com>
2026-07-18 14:20:51 +0900

    出力部分を記述 | codeを走らせた
    通常が消費税20円合計が220円 会員が消費税15円合計が165円になった

--- 20260529/12/task12.cpp
+++ 20260529/12/task12.cpp
@@ -13,4 +13,17 @@ int main() {
     int member_tax = MEMBER_PRICE * TAX_RATE;
     int member_total = MEMBER_PRICE + member_tax;
 
+    cout << " 通常価格 " << endl;
+    cout << "定価    : " << NORMAL_PRICE << " 円" << endl;
+    cout << "消費税額: " << normal_tax << " 円" << endl;
+    cout << "合計金額: " << normal_total << " 円" << endl;
+    cout << endl;
+
+    cout << " 会員価格 " << endl;
+    cout << "定価    : " << MEMBER_PRICE << " 円" << endl;
+    cout << "消費税額: " << member_tax << " 円" << endl;
+    cout << "合計金額: " << member_total << " 円" << endl;
+
+    return 0;
+
 }
